49356a9df879b57c5aa9cb8842724f63bb2ff089
Kevin Hang <herezy2006@gmail.com>
2022-11-11 18:16:43 +0100

    Faction (or brand) done

--- src/components/laser/Laser.jsx
+++ src/components/laser/Laser.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./laser.css";
 
+import { first_order, rebel, react, jedi, galactic,} from "./imports"
+
+
 const Laser = () => {
   return (
-    <div>Laser</div>
+    <div className="vador__laser section__padding">
+      <div>
+        <img src={first_order} alt="first_order" />
+      </div>
+      <div>
+        <img src={rebel} alt="rebel" />
+      </div>
+      <div>
+        <img src={react} alt="react" />
+      </div>
+      <div>
+        <img src={jedi} alt="jedi" />
+      </div>
+      <div>
+        <img src={galactic} alt="galactic" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/laser/imports.js
@@ -0,0 +1,13 @@
+import first_order from "../../assets/first_order.png";
+import rebel from "../../assets/rebel.png";
+import react from "../../assets/react.png";
+import jedi from "../../assets/jedi.png";
+import galactic from "../../assets/galactic.png";
+
+export {
+  first_order,
+  rebel,
+  react,
+  jedi,
+  galactic,
+}
