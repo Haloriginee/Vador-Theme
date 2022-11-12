d5374f3a061448b243849f1be828b0a06fdc451b
Kevin Hang <herezy2006@gmail.com>
2022-11-12 18:52:21 +0100

    call to action done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,19 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="vador__cta">
+      <div className="vador__cta-content">
+        <p>
+          “No … I am your Father…”
+        </p>
+        <h3>
+        “Come Join The Dark Side…”
+        </h3>
+      </div>
+      <div className="vador__cta-button">
+        <button type="button">Join</button>
+      </div>
+    </div>
   )
 }
 
