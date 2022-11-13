1d37ab4c1ae93006cf2a40044770fb7f1af12095
Kevin Hang <herezy2006@gmail.com>
2022-11-13 19:51:44 +0100

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Article} from "./article/Article";
-export { default as CTA} from "./cta/CTA";
-export { default as Laser} from "./laser/Laser";
-export { default as Navbar} from "./navbar/Navbar";
-export { default as Sith} from "./sith/Sith";
--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Blog} from "./blog/Blog";
-export { default as Features} from "./features/Features";
-export { default as Footer} from "./footer/Footer";
-export { default as Force} from "./force/Force";
-export { default as Header} from "./header/Header";
-export { default as Lightsaber} from "./lightsaber/Lightsaber";
