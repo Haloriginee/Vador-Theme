0a11a6bff9e61748ff14201f8fee174becb40dff
Kevin Hang <herezy2006@gmail.com>
2022-11-13 18:12:41 +0100

    blog session done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./article.css";
 
-const Article = () => {
+const Article = ( { imgUrl, date, title } ) => {
   return (
-    <div>Article</div>
+    <div className="vador__blog-container_article">
+      <div className="vador__blog-container_article-image">
+        <img src={imgUrl} alt="imgUrl" />
+      </div>
+      <div className="vador__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>read more(just getting some xp)</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import "./blog.css";
+import Article from "../../components/article/Article"
+import { blog01, blog02, blog03, blog04, blog05} from "./imports"
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <div classnName="vador__blog section__padding" id="blog">
+      <div className="vador__blog-heading">
+        <h1 className="gradient__text">
+          "Peace is a lie. There is only Passion. Through Passion, I gain Strength. Through Strength, I gain Power."
+        </h1>
+      </div>
+      <div className="vador__blog-container">
+        <div className="vador__blog-container_g1">
+          <Article imgUrl={blog01} date="01/01/2023" title="The Sith Empire will burn brightly, like a supernova, dwarfing the complacent Republic." />
+        </div>
+        <div className="vador__blog-container_g2">
+        <Article imgUrl={blog02} date="01/01/2023" title="The Sith Empire will burn brightly, like a supernova, dwarfing the complacent Republic." />
+        <Article imgUrl={blog03} date="01/01/2023" title="The Sith Empire will burn brightly, like a supernova, dwarfing the complacent Republic." />
+        <Article imgUrl={blog04} date="01/01/2023" title="The Sith Empire will burn brightly, like a supernova, dwarfing the complacent Republic." />
+        <Article imgUrl={blog05} date="01/01/2023" title="The Sith Empire will burn brightly, like a supernova, dwarfing the complacent Republic." />
+        </div>
+      </div>
+    </div>
   )
 }
 
